e65e5ec5c00876c0de7de9b64e68dcda026b042f
Mati <180681mvs@gmail.com>
2025-07-20 08:14:27 -0300

    cambio en la url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ['https://TU-NOMBRE.netlify.app', 'http://localhost:5173']
+  origin: ['https://peronperon.netlify.app', 'http://localhost:5173']
 }));
 
 app.use(express.json());
